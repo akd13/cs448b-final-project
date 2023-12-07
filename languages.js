f18b167c88ba8ff66446d88521426d5ae6bdd5ec
Ananya Kapoor <ananyaka@stanford.edu>
2023-12-07 20:31:34 +0530

    Data and changed viz1 draft

--- languages.js
+++ languages.js
@@ -1,227 +1,108 @@
-
-var selectedCountry = 'Choose birth country'
-const attributes_country = ['Choose birth country', 'Afghanistan', 'Albania', 'Algeria', 'Argentina', 'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bangladesh','Belarus','Belgium', 'Bolivia', 'Bosnia and Herzegovina', 'Brazil', 'Bulgaria', 'Cameroon', 'Canada', 'Chile', 'China', 'Colombia', 'Costa Rica', 'Cuba', 'Czech Republic', 'Denmark', 'Egypt', 'Ethiopia', 'Finland', 'France', 'Germany', 'Ghana', 'Greece', 'Guatemala', 'Haiti', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Israel', 'Italy', 'Japan', 'Jordan', 'Kenya', 'Kiribati', 'Latvia', 'Lebanon', 'Liberia', 'Malaysia', 'Mexico', 'Morocco', 'Netherlands', 'New Zealand', 'Nigeria', 'Norway', 'Pakistan', 'Palestine', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Russia', 'Serbia', 'Singapore', 'Somalia', 'South Africa', 'South Korea', 'Spain', 'Sudan', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Thailand', 'Uganda', 'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Venezuela', 'Zimbabwe' ];
-const select_country = d3.select("#attribute-selector-country");
-select_country.selectAll("option")
-    .data(attributes_country)
-    .enter()
-    .append("option")
-    .text(d => d)
-    .attr("value", d => d);
-
-function Legend(color, {
-    title,
-    tickSize = 6,
-    width = 320,
-    height = 44 + tickSize,
-    marginTop = 18,
-    marginRight = 0,
-    marginBottom = 16 + tickSize,
-    marginLeft = 50,
-    ticks = width / 180,
-    tickFormat,
-    tickValues
-  } = {}) {
-  
-    function ramp(color, n = 256) {
-      const canvas = document.createElement("canvas");
-      canvas.width = n;
-      canvas.height = 1;
-      const context = canvas.getContext("2d");
-      for (let i = 0; i < n; ++i) {
-        context.fillStyle = color(i / (n - 1));
-        context.fillRect(i, 0, 1, 1);
-      }
-      return canvas;
-    }
-  
-    const svg = d3.create("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .attr("viewBox", [0, 0, width, height])
-        .style("overflow", "visible")
-        .style("display", "block");
-  
-    let tickAdjust = g => g.selectAll(".tick line").attr("y1", marginTop + marginBottom - height);
-    let x;
-  
-    // Continuous
-    if (color.interpolate) {
-      const n = Math.min(color.domain().length, color.range().length);
-  
-      x = color.copy().rangeRound(d3.quantize(d3.interpolate(marginLeft, width - marginRight), n));
-  
-      svg.append("image")
-          .attr("x", marginLeft)
-          .attr("y", marginTop)
-          .attr("width", width - marginLeft - marginRight)
-          .attr("height", height - marginTop - marginBottom)
-          .attr("preserveAspectRatio", "none")
-          .attr("xlink:href", ramp(color.copy().domain(d3.quantize(d3.interpolate(0, 1), n))).toDataURL());
-    }
-  
-    // Sequential
-    else if (color.interpolator) {
-      x = Object.assign(color.copy()
-          .interpolator(d3.interpolateRound(marginLeft, width - marginRight)),
-          {range() { return [marginLeft, width - marginRight]; }});
-  
-      svg.append("image")
-          .attr("x", marginLeft)
-          .attr("y", marginTop)
-          .attr("width", width - marginLeft - marginRight)
-          .attr("height", height - marginTop - marginBottom)
-          .attr("preserveAspectRatio", "none")
-          .attr("xlink:href", ramp(color.interpolator()).toDataURL());
-  
-      // scaleSequentialQuantile doesn’t implement ticks or tickFormat.
-      if (!x.ticks) {
-        if (tickValues === undefined) {
-          const n = Math.round(ticks + 1);
-          tickValues = d3.range(n).map(i => d3.quantile(color.domain(), i / (n - 1)));
-        }
-        if (typeof tickFormat !== "function") {
-          tickFormat = d3.format(tickFormat === undefined ? ",f" : tickFormat);
-        }
-      }
-    }
-  
-    // Threshold
-    else if (color.invertExtent) {
-      const thresholds
-          = color.thresholds ? color.thresholds() // scaleQuantize
-          : color.quantiles ? color.quantiles() // scaleQuantile
-          : color.domain(); // scaleThreshold
-  
-      const thresholdFormat
-          = tickFormat === undefined ? d => d
-          : typeof tickFormat === "string" ? d3.format(tickFormat)
-          : tickFormat;
-  
-      x = d3.scaleLinear()
-          .domain([1, 72])
-          .rangeRound([marginLeft, width - marginRight]);
-  
-      svg.append("g")
-        .selectAll("rect")
-        .data(color.range())
-        .join("rect")
-          .attr("x", (d, i) => x(thresholds[i]))
-          .attr("y", marginTop)
-          .attr("width", (d, i) => x(thresholds[i + 1]) - x(thresholds[i]))
-          .attr("height", height - marginTop - marginBottom)
-          .attr("fill", d => d);
-  
-      tickValues = d3.range(thresholds.length);
-      tickFormat = i => thresholdFormat(thresholds[i], i);
-    }
-  
-    // Ordinal
-    else {
-      x = d3.scaleBand()
-          .domain(color.domain())
-          .rangeRound([marginLeft, width - marginRight]);
-  
-      svg.append("g")
-        .selectAll("rect")
-        .data(color.domain())
-        .join("rect")
-          .attr("x", x)
-          .attr("y", marginTop)
-          .attr("width", Math.max(0, x.bandwidth() - 1))
-          .attr("height", height - marginTop - marginBottom)
-          .attr("fill", color);
-  
-      tickAdjust = () => {};
-    }
-  
-    svg.append("g")
-        .attr("transform", `translate(50,${height - marginBottom})`)
-        .call(tickAdjust)
-        .call(g => g.select(".domain").remove())
-        .call(g => g.append("text")
-          .attr("x", 0)
-          .attr("y", marginTop + marginBottom - height - 6)
-          .attr("fill", "currentColor")
-          .attr("text-anchor", "start")
-          .attr("font-weight", "bold")
-          .attr("class", "title")
-          .text(title));
-  
-    return svg.node();
-  }
+var margin = { top: 30, right: 30, bottom: 70, left: 60 },
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
 
-d3.csv("data/ted_speakers_birth.csv").then(function (data) {
-
-  const legend = Legend(d3.scaleSqrt([1, 60], ["#FFFFFF", "#a9081a"]), {
-      title: "Number of Languages ->"
-  })
-  d3.select("#viz1-legend").node().appendChild(legend);
-
-  const svg = d3
+    const svg = d3
     .select("#viz1-svg")
     .append("svg")
       .attr("id", "viz1-svg-main")
-    .attr("width", '100vw')
-    .attr("height", '100vh');
+      .attr("width", '100vw')
+      .attr("height", '100vh')
+    .append("g")
+      .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
 
-  var myColor = d3.scaleLinear().domain([1, 72]).range(["#FFFFFF", "#a9081a"]);
-
-  function ticked(selectedCountry) {
-    svg
-      .selectAll("circle")
-      .data(data)
-      .join("circle")
-      .attr("r", (d) => {
-        if (d.views > 250000) {
-          return d.views / 250000;
-        } else {
-          return 1;
-        }
-      })
-      .attr("cx", (d) => d.x)
-      .attr("cy", (d) => d.y)
-      .attr("fill", (d) => {
-        if (selectedCountry === 'Choose birth country' || d.country === selectedCountry) {
-            return myColor(d.languages);
-        } else {
-            return "gray"; 
-        }
-      })
-      .append("title")
-      .text(
-        (d) =>
-          `Views: ${d.views} \n
-           Languages: ${d.languages}
-          `
-      );
-  }
-
-  select_country.on('change', function() {
-    selectedCountry = d3.select(this).property('value');
-    console.log(selectedCountry);
-    ticked(selectedCountry)
-});
- 
-    const element = document.getElementById('viz1');
-    const rect = element.getBoundingClientRect();
-    const centerX = rect.left + rect.width / 2;
-    const centerY = -50 + rect.top + rect.height / 2;
+    const svg2 = d3
+    .select("#viz2-svg")
+    .append("svg")
+      .attr("id", "viz2-svg-main")
+      .attr("width", '100vw')
+      .attr("height", '100vh')
+    .append("g")
+      .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
     
+d3.csv("data/occupation_views_averaged.csv").then(function (data) {
 
-    var simulation = d3
-    .forceSimulation(data)
-    .force("charge", d3.forceManyBody().strength(-0.5))
-    .force("center", d3.forceCenter(centerX, centerY))
-    .force(
-      "collision",
-      d3.forceCollide().radius((d) => d.views / 230000)
-    )
-    .on("tick", function () {
-      ticked(selectedCountry);
-    });
-});
+    var x = d3.scaleBand()
+  .range([ 0, width ])
+  .domain(data.map(function(d) { return d.Occupation_cluster; }))
+  .padding(0.2);
+svg.append("g")
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(x))
+  .selectAll("text")
+    .attr("transform", "translate(-10,0)rotate(-45)")
+    .style("text-anchor", "end")
+    .style("fill", "#fcdcbf");
+
+// Add Y axis
+var y = d3.scaleLinear()
+  .domain([0, 5613145])
+  .range([ height, 0]);
+svg.append("g")
+  .call(d3.axisLeft(y))
+  .selectAll("text")
+    .style("fill", "#fcdcbf");
+
+// Bars
+svg.selectAll("mybar")
+  .data(data)
+  .enter()
+  .append("rect")
+    .attr("x", function(d) { return x(d.Occupation_cluster); })
+    .attr("y", function(d) { return y(d.Views); })
+    .attr("width", x.bandwidth())
+    .attr("height", function(d) { return height - y(d.Views); })
+    .attr("fill", "red")
+    .on("mouseover", function(event, d) { 
+      d3.select(this).attr("fill", "#69b3a2")
+      var clusterName = "data/" + d.Occupation_cluster + ".csv"
+      showClusterDetails(clusterName);
+   })
+   .on("mouseout", function() { 
+    d3.select(this).attr("fill", "red")
+ });
+})
+
+function showClusterDetails(clusterName) {
+  svg2.selectAll("*").remove();
+  d3.csv(clusterName).then(function (data2) {
+
+  var x2 = d3.scaleBand()
+  .range([ 0, width ])
+  .domain(data2.map(function(d) { return d.Occupation; }))
+  .padding(0.2);
+svg2.append("g")
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(x2))
+  .selectAll("text")
+    .attr("transform", "translate(-10,0)rotate(-45)")
+    .style("text-anchor", "end")
+    .style("fill", "#fcdcbf");
+
+// Add Y axis
+var y2 = d3.scaleLinear()
+  .domain([0, 5613145])
+  .range([ height, 0]);
+svg2.append("g")
+  .call(d3.axisLeft(y2))
+  .selectAll("text")
+    .style("fill", "#fcdcbf");
+
+// Bars
+svg2.selectAll("mybar2")
+  .data(data2)
+  .enter()
+  .append("rect")
+    .attr("x", function(d) { return x2(d.Occupation); })
+    .attr("y", function(d) { return y2(d.Views); })
+    .attr("width", x2.bandwidth())
+    .attr("height", function(d) { return height - y2(d.Views); })
+    .attr("fill", "yellow")
+})
+}
 
 // navigate left and right
 document.addEventListener('keydown', function(event) {
